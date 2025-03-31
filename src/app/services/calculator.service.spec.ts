fe2e6fb5cbc2dc7198f57e15ce1519a35d0b0963
Aren-R <arenrepko7@gmail.com>
2025-03-31 21:34:53 +0200

    tests

---
+++ src/app/services/calculator.service.spec.ts
@@ -0,0 +1,34 @@
+import { CalculatorService } from './calculator.service';
+
+describe('CalculatorService', () => {
+  let service: CalculatorService;
+
+  beforeEach(() => {
+    service = new CalculatorService();
+  });
+
+  it ('should validate input correctly', () => {
+    expect(() => service.validateInput('1')).not.toThrowError();
+    expect(() => service.validateInput('G')).toThrowError();
+  });
+
+  it('should add two valid hexadecimal numbers', () => {
+    expect(service.add('A', '5')).toEqual('F');
+  });
+
+  it('should not allow input greater than 2 digits', () => {
+    expect(() => service.add('100', 'A')).toThrowError('Input exceeds 2 digits');
+  });
+
+  it('should return a maximum of 4 digits in output', () => {
+    expect(service.multiply('FF', 'F')).toEqual('EF1'); 
+  });
+
+  it('should not return negative values', () => {
+    expect(() => service.subtract('5', 'A')).toThrowError('Negative result not allowed');
+  });
+
+  it('should not return decimal values', () => {
+    expect(() => service.divide('A', '3')).toThrowError('Decimal results not allowed');
+  });
+});
